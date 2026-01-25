b02c52514fae40a1e1a978f24dc1c726779768e0
Volodymyr Oliinyk <oleynik.96@gmail.com>
2026-01-25 16:29:36 +0200

    fix: fix error messages during sigh up and sign in

--- src/main/java/app/demo/realworld/model/request/LoginRequest.java
+++ src/main/java/app/demo/realworld/model/request/LoginRequest.java
@@ -3,9 +3,11 @@ package app.demo.realworld.model.request;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @JsonRootName("user")
 public record LoginRequest(
         @NotBlank(message = "can't be empty") @Email(message = "invalid format") String email,
-        @NotBlank(message = "can't be empty") String password) {
+        @NotNull(message = "can't be empty") @Size(min = 6, max = 20) String password) {
 }
--- src/main/java/app/demo/realworld/model/request/RegistrationRequest.java
+++ src/main/java/app/demo/realworld/model/request/RegistrationRequest.java
@@ -3,11 +3,12 @@ package app.demo.realworld.model.request;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @JsonRootName("user")
 public record RegistrationRequest(
         @NotBlank(message = "can't be empty") @Email(message = "invalid format") String email,
-        @NotBlank(message = "can't be empty") @Size(min = 6, max = 20) String password,
-        @NotBlank(message = "can't be empty") @Size(min = 4, max = 20) String username) {
+        @NotNull(message = "can't be empty") @Size(min = 6, max = 20) String password,
+        @NotNull(message = "can't be empty") @Size(min = 4, max = 20) String username) {
 }
